b2dc1f33eebb1a709ab071f5d071e64e35d2de9e
A-Jones343 <alexj.career@gmail.com>
2025-08-23 18:52:46 -0500

    Successfully implemented password hashing and creating users.

--- GymApi/Controllers/UserController.cs
+++ GymApi/Controllers/UserController.cs
@@ -1,16 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
+using GymApi.Services;
+using GymApi.Models;
 
 namespace GymApi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
 public class UserController : ControllerBase {
-
-  [HttpPost(Name="create_user")]
-  public IActionResult Post([FromBody] User user) {
+    
+    private readonly UserService _service;
+    public UserController(UserService service) { _service = service; }
+    
+  // Creates user in db, responds with refresh and access tokens if successful request.
+  [HttpPost("create")]
+  public IActionResult Create([FromBody] User user) {
       if (!ModelState.IsValid) { return BadRequest(ModelState); }
-
+      _service.CreateUser(user);
+        
+      
+      
       return Ok("User Created Successfully");
+  }
+
+  // Updates user in db.
+  [HttpPost("update")]
+  public IActionResult Update([FromBody] User user) {
+    if (!ModelState.IsValid) { return BadRequest(ModelState); }
+    
+
+    return Ok("User Updated Successfully");
   }
 
 
