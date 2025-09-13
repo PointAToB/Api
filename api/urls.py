1797113b9df3b7f3371e081f5ba9e667007b25de
A-Jones343 <alexj.career@gmail.com>
2025-09-12 22:33:25 -0500

    Rewrote backend, started writing user endpoints

---
+++ api/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from ninja import NinjaAPI
+from user.router import router
+api = NinjaAPI()
+
+api.add_router('', router)
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', api.urls),
+]
