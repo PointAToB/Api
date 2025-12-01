be6448c8a9ce467f3ab2e117b859a7291119ba47
mwnahas <mostafalnahaass@gmail.com>
2025-12-01 16:25:08 -0600

    API

--- api/api.py
+++ api/api.py
@@ -2,6 +2,8 @@ from ninja_jwt.controller import NinjaJWTDefaultController
 from ninja_extra import NinjaExtraAPI
 from user.router import router
 from nutritionLog.router import router as nutrition_router
+from django.urls import path
+from workoutLog.views import router as workouts_router
 
 api = NinjaExtraAPI()
 api.register_controllers(NinjaJWTDefaultController)
@@ -9,3 +11,4 @@ api.register_controllers(NinjaJWTDefaultController)
 
 api.add_router('', router)
 api.add_router('nutrition/', nutrition_router)
+api.add_router("/workouts", workouts_router)
--- api/settings.py
+++ api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'user',
     'nutritionLog',
     'ninja_extra',
+    'exerciseList',
+    'workoutLog'
 ]
 
 MIDDLEWARE = [
@@ -74,14 +76,10 @@ ASGI_APPLICATION = 'api.asgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
-    'user': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    "default": dj_database_url.config()
 }
 
-DATABASE_ROUTERS = ['api.db_router.DbRouter']
+DATABASE_ROUTERS = []
 
 
 AUTH_USER_MODEL = 'user.User'
@@ -121,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
